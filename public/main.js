826860e58060aeea40edcc4543c2bed77281ccf7
Sergey Barsukov <barsukof@live.com>
2017-09-05 18:13:08 +0300

    простой сервер на ноде

---
+++ public/main.js
@@ -0,0 +1,6 @@
+const greeting = document.getElementsByClassName('greeting')[0];
+
+greeting.addEventListener('click', () => {
+    greeting.classList.add('hidden');
+}, false);
+
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+	res.send('404');
+});
+
+app.listen(process.env.PORT || '8080', () => {
+	console.log('listen')
+});
