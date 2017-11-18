0264530d6a38d065d776fa337d45b8ad8af4a722
Sergey Barsukov <barsukof@live.com>
2017-11-19 00:40:59 +0300

    service worker

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+"use strict";
+const CACHE_NAME = "serviceWorkerAlpha.1";
+
+const cacheUrls = [
+	'bundle.css',
+	'bundle.js'
+	//дописать все то то выгружаем на сайт (картинки и тд)
+];
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then((cache) => {
+				return cache.addAll(cacheUrls);
+			})
+	);
+});
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.match(event.request).then((cachedResponse) => {
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+			return fetch(event.request);
+		})
+	);
+});
