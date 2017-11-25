cfbc1af21e6e54de38e51edae7b4494e9e6792e9
Sergey Barsukov <barsukof@live.com>
2017-11-25 18:25:25 +0300

    fix service-worker path and added in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,63 @@
 const webpack = require('webpack');
 let path = require('path');
 
-module.exports = {
-    context: __dirname,
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist'),
-    },
-    entry: './app/application.js',
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader'
-            }, {
-                test: /\.scss$/,
-                use: [{
-                    loader: 'style-loader-es3' // creates style nodes from JS strings
+module.exports = [
+    {
+        context: __dirname,
+        entry: {
+            bundle: './app/application.js',
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.join(__dirname, 'dist'),
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader'
                 }, {
-                    loader: 'css-loader' // translates CSS into CommonJS
+                    test: /\.scss$/,
+                    use: [{
+                        loader: 'style-loader-es3' // creates style nodes from JS strings
+                    }, {
+                        loader: 'css-loader' // translates CSS into CommonJS
+                    }, {
+                        loader: 'sass-loader' // compiles Sass to CSS
+                    }]
                 }, {
-                    loader: 'sass-loader' // compiles Sass to CSS
-                }]
-            }, {
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=3000&name=images/[name].[ext]',
-            },
-            {
-                test: /\.(ttf)$/,
-                loader: 'url-loader?limit=3000&name=fonts/[name].[ext]',
-            },
-            {
-                test: /\.(babylon)$/,
-                loader: 'url-loader?limit=3000&name=meshes/[name].[ext]',
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader'
-            }, {
-                test: /\.(pug|jade)$/,
-                loader: 'pug-loader'
-            },
-        ],
-    },
-};
+                    test: /\.(png|jpg)$/,
+                    loader: 'url-loader?limit=3000&name=images/[name].[ext]',
+                },
+                {
+                    test: /\.(ttf)$/,
+                    loader: 'url-loader?limit=3000&name=fonts/[name].[ext]',
+                },
+                {
+                    test: /\.(babylon)$/,
+                    loader: 'url-loader?limit=3000&name=meshes/[name].[ext]',
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel-loader'
+                }, {
+                    test: /\.(pug|jade)$/,
+                    loader: 'pug-loader'
+                },
+            ],
+        },
+    }, {
+        context: __dirname,
+        entry: {
+            serviceworker: './app/modules/service-worker.js'
+        },
+        output: {
+            filename: 'service-worker.js',
+            path: path.join(__dirname, 'dist'),
+        },
+    }, 
+];
